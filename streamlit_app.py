b85025974e01532b7485f34d0d33b75e4bd899dd
Harrison He <hehar@bc.edu>
2025-06-26 16:14:52 +0000

    added imports

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 
+import pandas as pd
+import altair as alt
+
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
